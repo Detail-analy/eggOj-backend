ab40aa8d489c46d8548495b5fd918da8407b2aa4
Detail <12837776+quhaoliu@user.noreply.gitee.com>
2023-12-19 19:57:36 +0800

    后端模板初始化

---
+++ src/main/java/com/liu/yuoj/mapper/PostThumbMapper.java
@@ -0,0 +1,17 @@
+package com.liu.yuoj.mapper;
+
+import com.liu.yuoj.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/liu/yuoj/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.liu.yuoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.liu.yuoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
